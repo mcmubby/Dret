3d25711368c85083bcee295b9b965bfcf5f8c480
Mubarak <12859241+FireDeclan@users.noreply.github.com>
2020-07-03 10:40:52 +0100

    Project setup

---
+++ src/DretBlog.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DretBlog.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
