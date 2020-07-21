ae05dd2ecb0756468bea963813f4ec2a7ac06899
Mubarak <12859241+FireDeclan@users.noreply.github.com>
2020-07-21 14:25:26 +0100

    Pagination Spag code

--- src/DretBlog.Web/Controllers/BlogContentController.cs
+++ src/DretBlog.Web/Controllers/BlogContentController.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using System.Linq;
 using DretBlog.Web.Interfaces;
 using DretBlog.Web.Models.Blog;
+using JW;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DretBlog.Web.Controllers
@@ -14,7 +16,9 @@ namespace DretBlog.Web.Controllers
         {
             _blogContent = blogContent;
         }
-        public IActionResult Index()
+
+        [HttpGet]
+        public IActionResult Index(int p = 1)
         {
             var AllPosts = _blogContent.GetAll();
             var sortPosts = AllPosts.Select(result => new PostsList
@@ -26,9 +30,34 @@ namespace DretBlog.Web.Controllers
                 
             });
             var model = new BlogViewModel(){
-                Posts = sortPosts
+                Posts = sortPosts,
+                
             };
+            
+            var PageSet = SetPage(p,model.Posts);
+            var testpages = Pagination(PageSet,model.Posts);
+            model.Pager = PageSet;
+            model.PagedPost = testpages;
+
+            
+
+            // // get pagination info for the current page
+            // Pager = new Pager(dummyItems.Count(), p, PageSize, MaxPages);
+
+            // // assign the current page of items to the Items property
+            // Items = dummyItems.Skip((Pager.CurrentPage - 1) * Pager.PageSize).Take(Pager.PageSize);
             return View(model);
+        }
+        public IEnumerable<PostsList> Pagination(Pager Pager, IEnumerable<PostsList> Posts)
+        {
+           var PagedPost = Posts.Skip((Pager.CurrentPage - 1) * Pager.PageSize).Take(Pager.PageSize);
+           return PagedPost;
+        }
+
+        public Pager SetPage(int p, IEnumerable<PostsList> Posts)
+        {
+            var Pager = new Pager(Posts.Count(), p ,5, ((Posts.Count())/5));
+            return Pager;
         }
 
         
--- src/DretBlog.Web/Models/Blog/BlogViewModel.cs
+++ src/DretBlog.Web/Models/Blog/BlogViewModel.cs
@@ -1,9 +1,14 @@
 using System.Collections.Generic;
+using JW;
 
 namespace DretBlog.Web.Models.Blog
 {
     public class BlogViewModel
     {
         public IEnumerable<PostsList> Posts { get; set; }
+        public Pager Pager { get; set; }
+        public IEnumerable<PostsList> PagedPost { get; set; }
+
+        
     }
 }
